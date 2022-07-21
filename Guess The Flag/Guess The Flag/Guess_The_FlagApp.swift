ef1622a32423877773f144301c2907ab5e813c69
Aman <71437805+ItsMeAmanUpadhyay@users.noreply.github.com>
2022-07-22 00:13:30 +0530

    Add files via upload

---
+++ Guess The Flag/Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Aman Upadhyay on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
